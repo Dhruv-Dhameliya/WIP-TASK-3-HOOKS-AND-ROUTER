3ef7791f633dbd1e33ffbae29d58c3a67d76a788
Dhruv-Dhameliya <dhruvkumar1624@gmail.com>
2025-02-13 14:35:03 +0530

    first commit

---
+++ router/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="mx-auto w-full max-w-7xl">
+      <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">
+        About US
+      </h1>
+    </div>
+  );
+}
---
+++ router/src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div className="mx-auto w-full max-w-7xl">
+      <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">
+        Contact Us
+      </h1>
+    </div>
+  );
+}
